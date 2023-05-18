756703cca9b9d547bf89c1c2815e226dc82e0cd5
NeuroDong <dongjinzong@126.com>
2023-05-18 15:48:25 +0800

    NeuroDong

--- customKing/modeling/meta_arch/__init__.py
+++ customKing/modeling/meta_arch/__init__.py
@@ -5,6 +5,6 @@ from turtle import forward
 from .build import META_ARCH_REGISTRY, build_model  # isort:skip
 
 #Classfication model
-from .Image_classification.Resnext import Resnet20,Resnet110,Resnet18,Resnet34,Resnet50,Resnet101,Resnet152,ResNeXt29_8x64d,ResNeXt29_16x64d,ResNeXt50,ResNeXt101,Wide_resnet50_2,Wide_resnet101_2
+from .Image_classification.Resnet import Resnet20,Resnet110,Resnet18,Resnet34,Resnet50,Resnet101,Resnet152,ResNeXt29_8x64d,ResNeXt29_16x64d,ResNeXt50,ResNeXt101,Wide_resnet50_2,Wide_resnet101_2
 
 __all__ = list(globals().keys())
